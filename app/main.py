303be9a910b8786ebf240571e5dc97a683475816
ohyeon1002 <skyblueazure4@gmail.com>
2025-07-09 18:02:50 +0900

    CORS setting(temporary)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from .routers import auth, solve, modelcall
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/api")
 
